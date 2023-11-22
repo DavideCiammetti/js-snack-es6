1fed3d1ea87d1d50cf6cdce9260de5458cd9404d
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-22 16:53:02 +0100

    feat: milestone 1

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
+# esecuzuine Snack 1
+1. prima di tutto mi creo il mio array di oggetti.
+2. poi con un ciclo foreach ciclo gli ogetti e stampo in console la bici col peso maggiore.
+3. in fine creo un destructuring e lo uso insieme al template literal per stampare a schermo gli elementi 
+
 # Snack 2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
--- esercizio1/scriptUno.js
+++ esercizio1/scriptUno.js
@@ -1,1 +1,41 @@
 'use strict';
+// Race, Endurance e Aero
+const bici = [
+
+    {
+        'nome': 'Race',
+        'peso': 800
+    },
+    {
+        'nome': 'Endurance',
+        'peso': 1200
+    },
+    {
+        'nome': 'Aereo',
+        'peso': 750
+    }
+];
+
+let biciPiuPesante = bici[0]; 
+let nameBike = '';
+
+bici.forEach((element)=>{
+    if(element.peso> biciPiuPesante.peso){
+        biciPiuPesante = element;
+        nameBike = element.nome
+    }
+    
+   
+});
+   
+console.log(biciPiuPesante.peso);
+console.log( nameBike);
+let bikeScreen = document.createElement('div');  //document.createElement('div')
+let pesoBici = document.createElement('div');
+const container = document.querySelector('.container');
+
+bikeScreen.innerHTML =  `<div> <p>nome: ${nameBike}</p> </div>`;
+pesoBici.innerHTML =  `<div> <p>peso: ${biciPiuPesante.peso}</p> </div>`;
+
+container.append(bikeScreen);
+container.append(pesoBici);
