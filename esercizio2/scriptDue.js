cb721f0589fb366e4ce590d488160b3d68737220
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-22 17:39:44 +0100

    feat: numeri random per punti e falli

--- esercizio2/scriptDue.js
+++ esercizio2/scriptDue.js
@@ -33,3 +33,21 @@ const squadraDiCalcio = [
         'falliSubiti': 0
     }
 ];
+
+// creo viariabile per numeri random
+let randomFalli = Math.floor(Math.random() * 150);
+let  randomPunti = Math.floor(Math.random() * 150);
+
+// console.log(random);
+
+squadraDiCalcio.map((element)=>{
+    let {puntiFatti, falliSubiti} = squadraDiCalcio;
+    randomFalli = Math.floor(Math.random() * 150);
+    randomPunti = Math.floor(Math.random() * 150);
+    puntiFatti = randomPunti;
+    falliSubiti = randomFalli;
+
+    console.log(`i punti fatti sono: ${puntiFatti}`);
+    console.log(`i falli subiti sono: ${falliSubiti}`);
+    return element;
+});
